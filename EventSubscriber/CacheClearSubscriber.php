7cbb86eda2734551f15ad53d46934e24d7bc2537
Pablo Lozano <lozanomunarriz@gmail.com>
2020-11-10 09:34:48 +0100

    Add license to class

--- EventSubscriber/CacheClearSubscriber.php
+++ EventSubscriber/CacheClearSubscriber.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of Sulu Megamenu Bundle.
+ *
+ * (c) The Cocktail Expericence S.L.
+ *
+ *  This source file is subject to the MIT license that is bundled
+ *  with this source code in the file LICENSE.
+ */
+
 namespace TheCocktail\Bundle\MegaMenuBundle\EventSubscriber;
 
 use Sulu\Bundle\WebsiteBundle\Event\CacheClearEvent;
@@ -8,6 +17,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use TheCocktail\Bundle\MegaMenuBundle\Builder\MenuBuilder;
 
+/**
+ * @author Pablo Lozano <lozanomunarriz@gmail.com>
+ */
 class CacheClearSubscriber implements EventSubscriberInterface
 {
     private TagAwareCacheInterface $cache;
